12b8fe34adc76a551e128122699c55c5039ebd80
Kennedy Maombi <141538916+itzkennedydev@users.noreply.github.com>
2024-12-19 19:55:16 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  images: {
+    domains: ['res.cloudinary.com', 'apply.haroldschickensportsbar.com'],
+  },
 };
 
 export default nextConfig;
