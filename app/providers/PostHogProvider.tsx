96f75745e204c7779b1a3595c8a1e328423d7f3a
Kennedy Maombi <kennedy@itskennedy.dev>
2024-12-28 12:47:00 -0700

    added posthog

--- app/providers/PostHogProvider.tsx
+++ app/providers/PostHogProvider.tsx
@@ -2,23 +2,19 @@
 
 import { useEffect } from 'react'
 import posthog from 'posthog-js'
-import { usePathname, useSearchParams } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 
 export function PHProvider({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
-  const searchParams = useSearchParams()
 
   useEffect(() => {
     if (pathname) {
-      let url = window.origin + pathname
-      if (searchParams?.toString()) {
-        url = url + `?${searchParams.toString()}`
-      }
+      const url = window.location.href
       posthog.capture('$pageview', {
         $current_url: url,
       })
     }
-  }, [pathname, searchParams])
+  }, [pathname])
 
   return children
 } 
