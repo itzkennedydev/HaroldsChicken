c7f8c4ed47dc4e2f255c7bc3001299a096ef00aa
Kennedy Maombi <kennedy@itskennedy.dev>
2024-12-28 12:49:40 -0700

    added posthog

--- app/lib/posthog.ts
+++ app/lib/posthog.ts
@@ -2,14 +2,27 @@
 
 import posthog from 'posthog-js'
 
+const posthogClient = typeof window !== 'undefined' 
+  ? posthog 
+  : null
+
 if (typeof window !== 'undefined') {
-  posthog.init('phc_rza5B1ITU2c3ReDl4rhwUBv82zdyVZYATfZT3AkBHii', {
-    api_host: 'https://us.i.posthog.com',
-    person_profiles: 'identified_only',
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.debug()
-    },
-  })
+  const isLocalhost = window.location.hostname === 'localhost' || 
+                     window.location.hostname === '127.0.0.1'
+
+  if (!isLocalhost) {
+    posthog.init('phc_rza5B1ITU2c3ReDl4rhwUBv82zdyVZYATfZT3AkBHii', {
+      api_host: 'https://us.i.posthog.com',
+      persistence: 'localStorage',
+      autocapture: true,
+      capture_pageview: false, // We'll handle this manually
+      capture_pageleave: true,
+      disable_session_recording: false,
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === 'development') posthog.debug()
+      },
+    })
+  }
 }
 
-export { posthog } 
+export { posthogClient as posthog } 
