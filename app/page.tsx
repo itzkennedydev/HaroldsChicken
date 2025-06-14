dd892c969f99748d3e348d27be1acbc97ca5155f
Kennedy Maombi <kennedy@itskennedy.dev>
2025-06-14 10:53:29 -0500

    updated hero

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import { Footer } from "./components/Footer";
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      <Header />
+      <Header variant="white" />
       <Hero />
       <Featured />
       <Cravings />
