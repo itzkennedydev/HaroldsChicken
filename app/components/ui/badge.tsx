1bbb8debc3ce70ccabf737be650d5eaa8919137a
Kennedy Maombi <kennedy@itskennedy.dev>
2025-06-15 14:31:56 -0500

    update UI

--- app/components/ui/badge.tsx
+++ app/components/ui/badge.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "@/lib/utils"
+import { cn } from "@/app/lib/utils"
 
 const badgeVariants = cva(
   "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-red-700 focus:ring-offset-2",
